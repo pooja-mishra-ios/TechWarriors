c5a447f80f21575e3c9a33b8187551aee7e14ca3
pooja-mishra-ios <poojasharmanet87@gmail.com>
2019-07-29 20:15:38 +0530

    Settings

--- TechWarriors/Settings/SettingCell.swift
+++ TechWarriors/Settings/SettingCell.swift
@@ -8,22 +8,52 @@
 
 import UIKit
 
+protocol SettingCellDelegate : class {
+    func settingUpdated(cell: SettingCell, shouldEnable: Bool)
+}
+
 class SettingCell: UITableViewCell {
 
     @IBOutlet weak var titleLbl : UILabel!
+    @IBOutlet weak var subtitleLbl : UILabel!
     @IBOutlet weak var settingSwitch : UISwitch!
+    @IBOutlet weak var checkbox : UIButton!
+    weak var delegate : SettingCellDelegate?
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        settingSwitch.layer.cornerRadius = round(settingSwitch.frame.height) / 2
+        settingSwitch.transform = CGAffineTransform(scaleX: 0.81, y: 0.81)
+        checkbox.imageView?.tintColor = mainColor
     }
 
-    func setup(title: String, isOn: Bool, showSwitch: Bool) {
-        titleLbl.text = title
-        settingSwitch.isHidden = !showSwitch
-        if showSwitch {
-            settingSwitch.isOn = isOn
-        }
+    @IBAction func checkboxTapped() {
+        checkbox.isSelected = !checkbox.isSelected
+        delegate?.settingUpdated(cell: self, shouldEnable: checkbox.isSelected)
+    }
+    
+    @IBAction func switchTapped() {
+        delegate?.settingUpdated(cell: self, shouldEnable: settingSwitch.isOn)
+    }
+    
+    func setupWithSwitch(setting: [String : Any]) {
+        let isConfigurable : NSNumber = setting["configurable"] as! NSNumber
+        let isEnabled : NSNumber = setting["enabled"] as! NSNumber
+        titleLbl.text = setting["title"] as? String
+        subtitleLbl.text = setting["subtitle"] as? String
+        settingSwitch.isHidden = !isConfigurable.boolValue
+        settingSwitch.isOn = isEnabled.boolValue
+        checkbox.isHidden = true
+    }
+    
+    func setupWithCheckbox(setting: [String : Any]) {
+        let isConfigurable : NSNumber = setting["configurable"] as! NSNumber
+        let isEnabled : NSNumber = setting["enabled"] as! NSNumber
+        titleLbl.text = setting["title"] as? String
+        subtitleLbl.text = setting["subtitle"] as? String
+        checkbox.isHidden = !isConfigurable.boolValue
+        checkbox.isSelected = isEnabled.boolValue
+        settingSwitch.isHidden = true
     }
     
 }
--- TechWarriors/Settings/SettingsViewController.swift
+++ TechWarriors/Settings/SettingsViewController.swift
@@ -7,22 +7,76 @@
 //
 
 import UIKit
+import CoreLocation
 
-class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, SettingCellDelegate {
 
     @IBOutlet weak var tableView : UITableView!
 
+    var settingArr = [[String : Any]]()
+    
     let cellIdentifier = "SettingCell"
-    let footerHeight : CGFloat = 100
+    let footerHeight : CGFloat = 1
+    let headerHeight : CGFloat = 80
+    let rowHeight : CGFloat = 80
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UINib.init(nibName: "SettingCell", bundle: nil), forCellReuseIdentifier: cellIdentifier)
+        let tableBgView = UIView(frame: tableView.bounds)
+        tableBgView.backgroundColor = UIColor.white
+        tableView.backgroundView = tableBgView
+        setData()
+    }
+    
+    func setData() {
+        //notification setting
+        var notificationSettings = [[String : Any]]()
+        let notifSetting1 = ["title" : "Allow Notifications", "subtitle" : "Allow notifications from admin", "configurable" : NSNumber(value: true), "enabled" : NSNumber(value: Preferences.shared().getPreference(key: notificationKey))] as [String : Any]
+        notificationSettings.append(notifSetting1)
+        let notifSetting2 = ["title" : "Allow Notifications After Logout", "subtitle" : "You will receive notification after logout", "configurable" : NSNumber(value: true), "enabled" : NSNumber(value: Preferences.shared().getPreference(key: notificationAfterLogoutKey))] as [String : Any]
+        notificationSettings.append(notifSetting2)
+        let notifSetting3 = ["title" : "Send Notification", "subtitle" : "Send otification to employees", "configurable" : NSNumber(value: false), "enabled" : NSNumber(value: false)] as [String : Any]
+        notificationSettings.append(notifSetting3)
+        settingArr.append(["Notification" : notificationSettings])
+        
+        //alarm setting
+        var alarmSettings = [[String : Any]]()
+        let alarmSetting1 = ["title" : "Enable Alarm", "subtitle" : "Enable alarm in emergency", "configurable" : NSNumber(value: true), "enabled" : NSNumber(value: Preferences.shared().getPreference(key: alarmKey))] as [String : Any]
+        alarmSettings.append(alarmSetting1)
+        settingArr.append(["Emergency Alarm" : alarmSettings])
+        
+        //location setting
+        var locationSettings = [[String : Any]]()
+        let locationSetting1 = ["title" : "Share My Location", "subtitle" : "Allow to share location", "configurable" : NSNumber(value: true), "enabled" : NSNumber(value: Preferences.shared().getPreference(key: locationKey))] as [String : Any]
+        locationSettings.append(locationSetting1)
+        settingArr.append(["Location" : locationSettings])
+        
+        //general setting
+        var generalSettings = [[String : Any]]()
+        let generalSetting1 = ["title" : "Logout", "subtitle" : "", "configurable" : NSNumber(value: false), "enabled" : NSNumber(value: false)] as [String : Any]
+        generalSettings.append(generalSetting1)
+        settingArr.append(["General" : generalSettings])
     }
     
     // MARK: - UITableView
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return settingArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return headerHeight
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: headerHeight))
+        let titleLabel = UILabel(frame: CGRect(x: 20, y: 0, width: header.frame.width-40, height: headerHeight))
+        titleLabel.font = regularFont16
+        titleLabel.textColor = mainColor
+        let mainDict : [String : Any] = settingArr[section] as [String : Any]
+        titleLabel.text = mainDict.keys[mainDict.keys.startIndex]
+        header.addSubview(titleLabel)
+        return header
     }
     
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
@@ -31,39 +85,73 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         let footer = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: footerHeight))
-        
-        let logoutBtn = UIButton(type: .custom)
-        logoutBtn.frame = CGRect(x: 20, y: (footerHeight-40)/2, width: (tableView.frame.width - 40), height: 40)
-        logoutBtn.setTitle("LOG OUT", for: .normal)
-        logoutBtn.titleLabel?.font = regularFont18
-        logoutBtn.layer.borderWidth = 1
-        logoutBtn.layer.borderColor = mainColor.cgColor
-        logoutBtn.setTitleColor(mainColor, for: .normal)
-        logoutBtn.addTarget(self, action: #selector(logout), for: .touchUpInside)
-        footer.addSubview(logoutBtn)
-        
+        footer.backgroundColor = (section == tableView.numberOfSections-1) ? UIColor.white : UIColor.lightGray.withAlphaComponent(0.5)
         return footer
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        let mainDict : [String : Any] = settingArr[section] as [String : Any]
+        let settings : [[String : Any]] = mainDict.values[mainDict.values.startIndex] as! [[String : Any]]
+        return settings.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
+        return rowHeight
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell : SettingCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! SettingCell
-        if indexPath.row == 0 {
-            cell.setup(title: "Passcode Lock", isOn: false, showSwitch: true)
+        cell.delegate = self
+        let mainDict : [String : Any] = settingArr[indexPath.section] as [String : Any]
+        let settings : [[String : Any]] = mainDict.values[mainDict.values.startIndex] as! [[String : Any]]
+        let setting = settings[indexPath.row]
+        if (indexPath.section == 0) {
+            cell.setupWithSwitch(setting: setting)
+        } else {
+            cell.setupWithCheckbox(setting: setting)
         }
+        cell.selectionStyle = .none
         return cell
     }
     
-    @objc func logout() {
-        Preferences.shared().removePreference(key: loginKey)
-        Utilily.showLogin()
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //logout
+        if (indexPath.section == tableView.numberOfSections-1) {
+            Preferences.shared().removePreference(key: loginKey)
+            Preferences.shared().removePreference(key: notificationKey)
+            Preferences.shared().removePreference(key: notificationAfterLogoutKey)
+            Preferences.shared().removePreference(key: alarmKey)
+            Preferences.shared().removePreference(key: locationKey)
+            LocationManager.shared().stop()
+            Utilily.showLogin()
+        }
     }
     
+    // MARK: - SettingCellDelegate
+    func settingUpdated(cell: SettingCell, shouldEnable: Bool) {
+        if let indexPath = tableView.indexPath(for: cell) {
+            if indexPath.section == 0 {
+                if indexPath.row == 0 {
+                    Preferences.shared().setPreference(value: NSNumber(value: shouldEnable), key: notificationKey)
+                } else {
+                    Preferences.shared().setPreference(value: NSNumber(value: shouldEnable), key: notificationAfterLogoutKey)
+                }
+            } else if indexPath.section == 1 {
+                Preferences.shared().setPreference(value: NSNumber(value: shouldEnable), key: alarmKey)
+            } else if indexPath.section == 2 {
+                if shouldEnable {
+                    if CLLocationManager.authorizationStatus() == .denied {
+                        Utilily.showErrorAlert(message: "Please turn on location to enable this feature")
+                    } else {
+                        Preferences.shared().setPreference(value: NSNumber(value: false), key: locationKey)
+                        LocationManager.shared().start()
+                    }
+                } else {
+                    LocationManager.shared().stop()
+                    Preferences.shared().setPreference(value: NSNumber(value: shouldEnable), key: locationKey)
+                }
+            }
+            UserDefaults.standard.synchronize()
+        }
+    }
 }
